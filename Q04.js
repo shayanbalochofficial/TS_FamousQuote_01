6c51b646306f009f570ccaa9682849ff2fc05cb4
shayanbalochofficial <144522960+shayanbalochofficial@users.noreply.github.com>
2024-04-04 01:53:14 +0500

    Initial commit

---
+++ Q04.js
@@ -0,0 +1,5 @@
+"use strict";
+// Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author.
+let author = "Albert Einstein";
+let quote = "A person who never made a mistake never tried anything new.";
+console.log(`${author} once said, "${quote}"`);
---
+++ Q04.ts
@@ -0,0 +1,5 @@
+// Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author.
+
+let author = "Albert Einstein";
+let famousquote = "A person who never made a mistake never tried anything new.";
+console.log(`${author} once said, "${famousquote}"`);
